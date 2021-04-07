c9ecb1d0029791f4e7c20e3bc2f278b5e7e38866
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-04-07 22:04:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,12 @@
+
 class Dog
-  def initialize(name, breed)
-    @name = name
-    @breed = "Mutt"
-  end
- 
-  def name=(name)
-    @name = name
-  end
- 
-  def name
-    @name
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
+
+	def initialize (name, breed = "Mutt")
+		@name = name
+		@breed = breed
+	end
+
+  attr_accessor :name
+  attr_accessor :breed
+
 end
